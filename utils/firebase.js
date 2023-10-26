f7b1fabf97f3d0af09774515b15d043fcfbe0847
ibrahem hassan <hibrahem266@gmail.com>
2023-10-26 15:14:59 +0300

    notfications

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,6 +1,6 @@
 import {initializeApp} from "firebase/app";
 import localforage from "localforage";
-import {getMessaging, getToken} from "firebase/messaging";
+import {getMessaging, getToken, onMessage} from "firebase/messaging";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
@@ -18,51 +18,35 @@ const app = initializeApp(firebaseConfig);
 export const authenti = getAuth(app);
 export const db = getFirestore(app)
 export const storage  = getStorage(app)
-
-const firebaseCloudMessaging = {
-    init: async () => {
-        // Initialize the Firebase app with the credentials
-        
-
-
-        try {
-            const messaging = getMessaging(app);
-            //console.log(messaging);
-            const tokenInLocalForage = await localforage.getItem("fcm_token");
-
-            // Return the token if it is alredy in our local storage
-            if (tokenInLocalForage !== null) {
-                return tokenInLocalForage;
+const messaging = getMessaging(app);
+export const requestPermission = () => {
+    console.log("Req user peimitions...");
+    Notification.requestPermission().then((permission) => {
+      if (permission === "granted") {
+        console.log("Notification User Permission Granted");
+        return getToken(messaging, {
+          vapidKey:
+          "BJepALF0a95YllxgoWS5qnbdvwlURFKW398sHWJQ8WGZ6xFIFA7Ore8aE0WTgHex-iOWzk4JSM541d1se1p_YNQ",
+        })
+          .then((currentToken) => {
+            if (currentToken) {
+              console.log("client Token ", currentToken);
+            } else {
+              console.log("Faild to Generate the app req token.");
             }
-
-            // Request the push notification permission from browser
-            const status = await Notification.requestPermission();
-            if (status && status === "granted") {
-                // Get new token from Firebase
-
-                getToken(messaging, {vapidKey: "BF6L1dWRxUP18FJUM_jbgICDSnPAlBL_PY8N1zFbue3YHX9x_yxmCVYUvo156HtwP3hAhPPHLrgYuuAle6cyAds"}).then((currentToken) => {
-                    if (currentToken) {
-                        // Send the token to your server and update the UI if necessary
-                        // ...
-                        // console.log(currentToken);
-                        localforage.setItem("fcm_token", currentToken);
-                        return currentToken;
-                    } else {
-                        // Show permission request UI
-                        console.log('No registration token available. Request permission to generate one.');
-                        // ...
-                    }
-                }).catch((err) => {
-                    console.log('An error occurred while retrieving token. ', err);
-                    // ...
-                });
-                // Set token in our local storage
-
-            }
-        } catch (error) {
-            console.error(error);
-            return null;
-        }
-    },
-};
-export {firebaseCloudMessaging};
+          })
+          .catch((err) => {
+            console.log("an error occure when req to receive the token", err);
+          });
+      } else {
+        console.log("User Permission Denied");
+      }
+    });
+  };
+  requestPermission();
+  export const onMessageListener = () =>
+    new Promise((resolve) => {
+      onMessage(messaging, (payload) => {
+        resolve(payload);
+      });
+    });
